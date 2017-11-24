01441d7739e4123333977d589d44a8340bcae31d
urpylka <urpylka@gmail.com>
2017-11-24 17:55:40 +0000

    Add arming protect

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ros_px4logs
-ROS package for download & upload PX4 logs to logs.px4.io
+ROS package (by pubsub technology) for download & upload PX4 logs to logs.px4.io
 ___
 
 For use this script bundle your PixHawk must connect to Companation Computer with USB (NOT UART!)
--- src/manager.py
+++ src/manager.py
@@ -2,34 +2,21 @@
 # -*- coding: utf-8 -*-
 # vim:set ts=4 sw=4 et:
 
-
+import os.path, rospy, time, requests
 from json_database import JSONDatabase
-import os.path
-import rospy
-import time
 from mavros_msgs.msg import State
 from mavros_msgs.srv import FileList
-from threading import Thread, Lock
-import requests
+from threading import Thread, Lock, Event
 from mavros.utils import *
 from mavros.nuttx_crc32 import *
 from mavros import ftp # /opt/ros/kinetic/lib/python2.7/dist-packages/mavros/ftp.py
-
-
-stop_operation = False
-
-def arming_protect(data):
-    if data.armed and ARMING_PROTECT:
-        stop_operation = True
-    else:
-        stop_operation = False
 
 
 def convert_to_rpi_path(log_directory, path_on_px4):
     return log_directory + '/' + os.path.basename(os.path.dirname(path_on_px4)).replace('-','') + '_' + os.path.basename(path_on_px4).replace('_','')
 
 
-def downloader(db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock):
+def downloader(db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock, accept_operation):
     print("Created downloader")
     """
     По-хорошему, нужна проверка что лог уже не скачен, и имя не повторяется
@@ -45,7 +32,7 @@ def downloader(db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock):
             try:
                 with mavftp_lock:
                     time.sleep(1)
-                    download(log['path_on_px4'], path_on_rpi, True, True, not _CHECK_FILE_CHECKSUM)
+                    download(log['path_on_px4'], path_on_rpi, accept_operation, True, True, not _CHECK_FILE_CHECKSUM)
                 # если успешно донладим, меняем флаг в словаре в памяти
                 db.on_download(log, path_on_rpi)
             except Exception as ex:
@@ -56,13 +43,16 @@ def downloader(db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock):
                 # вообще можно перейти к другому элементу из очереди
 
 
-def uploader(db, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL):
+def uploader(db, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL, accept_operation):
     print("Created uploader")
     while True: #тк демон
         log = db.uq.get()
 
         while not log['uploaded']:
             try:
+
+                accept_operation.wait()
+
                 # хорошая идея, если развертывать свой logs.px4.io можно возвращаться как-то еще ссылку на удаление лога
                 status_code, log_url = upload(os.path.basename(log['path_on_rpi']), _USER_FEEDBACK, log['path_on_rpi'], _USER_EMAIL, _UPLOADER_URL)
                 if status_code == 200:
@@ -79,7 +69,7 @@ def uploader(db, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL):
 ftp_list_call = rospy.ServiceProxy('/mavros/ftp/list', FileList)
 
 
-def finder(db, _FINDER_INTERVAL, mavftp_lock):
+def finder(db, _FINDER_INTERVAL, mavftp_lock, accept_operation):
     print("Created finder")
     """
     Функция поиска новых логов на PX4.
@@ -90,28 +80,31 @@ def finder(db, _FINDER_INTERVAL, mavftp_lock):
     while True:
         try:
             with mavftp_lock:
+
+                accept_operation.wait()
+
                 time.sleep(1) # чтобы поток успел умереть
                 print("Ищу новые логи...")
                 logs_folder = ftp_list_call("/fs/microsd/log")
                 if logs_folder.success and logs_folder.r_errno == 0:
-        
+
                     # проход по папкам с файлами логов
                     for logs_folder_list in logs_folder.list:
-        
+
                         # если type равен 0, значит это файл, если 1, то это папка
                         if logs_folder_list.type == 1:
-        
+
                             path_session_logs_folder = "/fs/microsd/log/" + logs_folder_list.name
-        
+
                             session_logs_folder = ftp_list_call(path_session_logs_folder)
                             if session_logs_folder.success and session_logs_folder.r_errno == 0:
-        
+
                                 for log in session_logs_folder.list:
                                     if (log.type == 0):
                                         # если найден файл лога
                                         log_path = path_session_logs_folder + "/" + log.name
                                         if not db.is_log_in_records(log_path): db.on_find(log_path)
-        
+
                             else: print("Import log folder error in " + path_session_logs_folder)
                 else: print("Import log folder error in /fs/microsd/log")
         except Exception as ex:
@@ -121,22 +114,22 @@ def finder(db, _FINDER_INTERVAL, mavftp_lock):
         time.sleep(_FINDER_INTERVAL)
 
 
-def create_downloaders(db, count, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock):
+def create_downloaders(db, count, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock, accept_operation):
     for i in range(count):
-      t = Thread(target = downloader, args = (db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock, ))
+      t = Thread(target = downloader, args = (db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock, accept_operation, ))
       t.daemon = True
       t.start()
 
 
-def create_uploaders(db, count, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL):
+def create_uploaders(db, count, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL, accept_operation):
     for i in range(count):
-      t = Thread(target = uploader, args = (db, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL, ))
+      t = Thread(target = uploader, args = (db, _USER_EMAIL, _USER_FEEDBACK, _UPLOADER_URL, accept_operation, ))
       t.daemon = True
       t.start()
 
 
-def create_finder(db, _FINDER_INTERVAL, mavftp_lock):
-    t = Thread(target = finder, args = (db, _FINDER_INTERVAL, mavftp_lock, ))
+def create_finder(db, _FINDER_INTERVAL, mavftp_lock, accept_operation):
+    t = Thread(target = finder, args = (db, _FINDER_INTERVAL, mavftp_lock, accept_operation, ))
     t.daemon = True
     t.start()
 
@@ -145,15 +138,14 @@ def create_finder(db, _FINDER_INTERVAL, mavftp_lock):
 # mavftp.do_download_local("/fs/microsd/log/2017-10-20/10_53_02.ulg","logs/10_53_02.ulg",True,True,True)
 
 
-def download(file_path,file_name,verbose=True,no_progressbar=False,no_verify=False):
+def download(file_path, file_name, accept_operation, verbose=True, no_progressbar=False, no_verify=False):
     # optimized transfer size for FTP message payload
     # XXX: bug in ftp.cpp cause a doubling request of last package.
     # -1 fixes that.
-    #FTP_CHUNK = 239 * 18 - 1
-    FTP_CHUNK = 500 * 1000 - 1
+    FTP_CHUNK = 239 * 18 - 1
 
     mavros.set_namespace("/mavros")
-    
+
     local_crc = 0
     local_file = open(file_name, 'wb')
 
@@ -164,7 +156,9 @@ def download(file_path,file_name,verbose=True,no_progressbar=False,no_verify=Fal
         #remote_file = ftp.open(file_path, 'r')
         with ftp.open(file_path, 'r') as remote_file:
             while True:
-                #if stop_operation: raise Exception("ARMING: Скачивание лога " + file_path + " приостановлено")
+
+                accept_operation.wait()
+
                 buf = remote_file.read(FTP_CHUNK)
                 if len(buf) == 0:
                     break
@@ -190,7 +184,7 @@ def upload(description, additional_feedback, file_path, email, uploader_url, all
     r = requests.post(uploader_url, files = files, data = values)
 
     return r.status_code, r.url
-    #if stop_operation: raise Exception("ARMING: Скачивание лога " + file_path + " приостановлено")
+
 
 def main():
     rospy.init_node('px4logs_manager')
@@ -207,15 +201,30 @@ def main():
     UPLOADER_URL = rospy.get_param('~uploader_url', "https://logs.px4.io/upload")
 
     rospy.loginfo('Inited px4logs_manager')
-    
+
+
+    accept_operation = Event()
+    accept_operation.set()
+
+    def arming_protect(data):
+        if data.armed and ARMING_PROTECT:
+            if accept_operation.is_set():
+                accept_operation.clear()
+                print("ARMING все операции кроме upload заблокированы")
+        else:
+            if not accept_operation.is_set():
+                accept_operation.set()
+                print("DISARMING все операции разблокированы")
+
+
     rospy.Subscriber('/mavros/state', State, arming_protect)
     mavftp_lock = Lock()
-    
+
     db = JSONDatabase(DB_JSON_PATH)
-    create_downloaders(db, DOWNLOADERS_COUNT, LOGS_DIRECTORY, CHECK_FILE_CHECKSUM, mavftp_lock)
-    create_uploaders(db, UPLOADERS_COUNT, USER_EMAIL, USER_FEEDBACK, UPLOADER_URL)
-    create_finder(db, FINDER_INTERVAL, mavftp_lock)
-    
+    create_downloaders(db, DOWNLOADERS_COUNT, LOGS_DIRECTORY, CHECK_FILE_CHECKSUM, mavftp_lock, accept_operation)
+    create_uploaders(db, UPLOADERS_COUNT, USER_EMAIL, USER_FEEDBACK, UPLOADER_URL, accept_operation)
+    create_finder(db, FINDER_INTERVAL, mavftp_lock, accept_operation)
+
     rospy.spin()
     #db.dq.join()
     #db.uq.join()
