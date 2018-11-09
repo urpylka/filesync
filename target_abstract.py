b98d393681546d67a971e243d85aea0f5959be00
Artem Smirnov <urpylka@gmail.com>
2018-11-09 16:51:29 +0300

    Fixes

---
+++ target_abstract.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:set ts=4 sw=4 et:
+
+from threading import Event
+
+class Target(object):
+
+    is_remote_available = Event()
+    db_fileds = []
+    verbose = False
+
+    def __init__(self, *args):
+        raise NotImplementedError()
+
+    def upload(self, local_path, remote_path):
+        """
+        Must return bool
+        """
+        raise NotImplementedError()
+
+    # Может лучше сделать Grabber or Deleter
+    # def del_source_file(self, remote_path, local_path, verbose = True):
+    #     pass
