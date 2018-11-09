81b89d54b1286dc05113b9a1f1e2b69601fd6beb
Artem Smirnov <urpylka@gmail.com>
2018-11-09 16:51:29 +0300

    Fixes

---
+++ source_abstract.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:set ts=4 sw=4 et:
+
+from threading import Event
+
+class Source(object):
+
+    is_remote_available = Event()
+    db_fileds = []
+    verbose = False
+
+    def __init__(self, *args):
+        raise NotImplementedError()
+
+    def get_list_of_files(self):
+        raise NotImplementedError()
+    
+    def download(self, remote_path, local_path):
+        raise NotImplementedError()
+
+    def del_source_file(self, remote_path, local_path):
+        raise NotImplementedError()
