5499be70c6bf8a4f8f2964ad685d8fe98285be8f
Artem Smirnov <urpylka@gmail.com>
2018-11-21 17:40:44 +0300

    Move code

--- src/source_logs_mavlink.py
+++ src/source_logs_mavlink.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim:set ts=4 sw=4 et:
 
-# Copyright 2017-2018 Smirnov Artem.
+# Copyright 2017-2018 Smirnov Artem
 
 import rospy, time, requests, mavros, os
 from json_database import JSONDatabase
@@ -62,8 +62,21 @@ class ProgressBar:
             self.pbar.finish()
 
 
-def do_download_local(file_path,file_name,verbose=True,no_progressbar=False,no_verify=False):
+def download(file_path, file_name, accept_operation, verbose=True, no_progressbar=False, no_verify=False):
     """
+    optimized transfer size for FTP message payload
+    XXX: bug in ftp.cpp cause a doubling request of last package.
+    -1 fixes that.
+    Размер чанка особо не влияет на скорость порядка 36Kb
+
+    Нужна общая блокировка при работе с mavftp
+     with mavftp_lock:
+        time.sleep(1)
+        size_on_px4 = download(log['path_on_px4'], path_on_rpi, accept_operation, True, True, not _CHECK_FILE_CHECKSUM)
+
+    может быть ошибка что флешка на пиксе не доступна (ошибка 110 например)
+    закрыть поток на ftp "rosservice call /mavros/ftp/close NAME_OF_FILE" & сбросить ftp "rosservice call /mavros/ftp/reset"    
+
     AttributeError: __exit__
     mavftp.do_download_local("/fs/microsd/log/2017-10-20/10_53_02.ulg","logs/10_53_02.ulg",True,True,True)
 
@@ -137,126 +150,6 @@ def do_download_local(file_path,file_name,verbose=True,no_progressbar=False,no_v
     rospy.spin()
 
 
-    # AttributeError: __exit__
-    # mavftp.do_download_local("/fs/microsd/log/2017-10-20/10_53_02.ulg","logs/10_53_02.ulg",True,True,True)
-
-
-    """
-    Функция поиска новых логов на PX4.
-    Тк пикс не создает супервложенных директорий,
-    можно обойтись без рекурсивного перехода между папками
-    """
-    while True:
-        try:
-            with mavftp_lock:
-
-                accept_operation.wait()
-
-                time.sleep(1) # чтобы поток успел умереть
-                print("Ищу новые логи...")
-                logs_folder = ftp_list_call("/fs/microsd/log")
-                if logs_folder.success and logs_folder.r_errno == 0:
-
-                    # проход по папкам с файлами логов
-                    for logs_folder_list in logs_folder.list:
-
-                        # если type равен 0, значит это файл, если 1, то это папка
-                        if logs_folder_list.type == 1:
-
-                            path_session_logs_folder = "/fs/microsd/log/" + logs_folder_list.name
-
-                            session_logs_folder = ftp_list_call(path_session_logs_folder)
-                            if session_logs_folder.success and session_logs_folder.r_errno == 0:
-
-                                for log in session_logs_folder.list:
-                                    if (log.type == 0):
-                                        # если найден файл лога
-                                        log_path = path_session_logs_folder + "/" + log.name
-                                        if not db.is_log_in_records(log_path): db.on_find(log_path)
-
-                            else: print("Import log folder error in " + path_session_logs_folder)
-                else: print("Import log folder error in /fs/microsd/log")
-        except Exception as ex:
-            print("Finder error: " + str(ex))
-
-
-def convert_to_rpi_path(log_directory, path_on_px4):
-    return log_directory + '/' + os.path.basename(os.path.dirname(path_on_px4)).replace('-','') + '_' + os.path.basename(path_on_px4).replace('_','')
-
-
-def downloader(db, _LOGS_DIRECTORY, _CHECK_FILE_CHECKSUM, mavftp_lock, accept_operation):
-    print("Created downloader")
-    """
-    По-хорошему, нужна проверка что лог уже не скачен, и имя не повторяется
-    https://gist.github.com/urpylka/0b0c2dcd55141ae1735df00b78f713fb
-    Кароче качать можно и нужно вообще побайтно (в принципе как я понял можно уже в полете), но туповато это самому реализовывать поэтому возьму это из mavftp
-    """
-    while True:
-        log = db.dq.get()
-        path_on_rpi = convert_to_rpi_path(_LOGS_DIRECTORY, log['path_on_px4'])
-        while not log['downloaded']:
-            try:
-                with mavftp_lock:
-                    time.sleep(1)
-                    size_on_px4 = download(log['path_on_px4'], path_on_rpi, accept_operation, True, True, not _CHECK_FILE_CHECKSUM)
-                # если успешно донладим, меняем флаг в словаре в памяти
-                db.on_download(log, path_on_rpi, size_on_px4)
-            except Exception as ex:
-                # может быть ошибка что флешка на пиксе не доступна (ошибка 110 например)
-                print("Downloader error: " + str(ex))
-                # закрыть поток на ftp "rosservice call /mavros/ftp/close NAME_OF_FILE" & сбросить ftp "rosservice call /mavros/ftp/reset"    
-                time.sleep(2)
-                # вообще можно перейти к другому элементу из очереди
-
-
-def finder(db, _FINDER_INTERVAL, mavftp_lock, accept_operation, ftp_list_call):
-    print("Created finder")
-    """
-    Функция поиска новых логов на PX4.
-    Тк пикс не создает супервложенных директорий,
-    можно обойтись без рекурсивного перехода между папками
-    """
-    while True:
-        try:
-            with mavftp_lock:
-
-                accept_operation.wait()
-
-                time.sleep(1) # чтобы поток успел умереть
-                print("Ищу новые логи...")
-                logs_folder = ftp_list_call("/fs/microsd/log")
-                if logs_folder.success and logs_folder.r_errno == 0:
-
-                    # проход по папкам с файлами логов
-                    for logs_folder_list in logs_folder.list:
-
-                        # если type равен 0, значит это файл, если 1, то это папка
-                        if logs_folder_list.type == 1:
-
-                            path_session_logs_folder = "/fs/microsd/log/" + logs_folder_list.name
-
-                            session_logs_folder = ftp_list_call(path_session_logs_folder)
-                            if session_logs_folder.success and session_logs_folder.r_errno == 0:
-
-                                for log in session_logs_folder.list:
-                                    if (log.type == 0):
-                                        # если найден файл лога
-                                        log_path = path_session_logs_folder + "/" + log.name
-                                        if not db.is_log_in_records(log_path): db.on_find(log_path)
-
-                            else: print("Import log folder error in " + path_session_logs_folder)
-                else: print("Import log folder error in /fs/microsd/log")
-        except Exception as ex:
-            print("Finder error: " + str(ex))
-            #time.sleep(2)
-            #continue
-        time.sleep(_FINDER_INTERVAL)
-
-def download(file_path, file_name, accept_operation, verbose=True, no_progressbar=False, no_verify=False):
-    # optimized transfer size for FTP message payload
-    # XXX: bug in ftp.cpp cause a doubling request of last package.
-    # -1 fixes that.
-    # Размер чанка особо не влияет на скорость порядка 36Kb
     FTP_CHUNK = 239 * 18 - 1
 
     mavros.set_namespace("/mavros")
@@ -295,28 +188,128 @@ def download(file_path, file_name, accept_operation, verbose=True, no_progressba
         ftp.reset_server()
 
 
-def upload(description, additional_feedback, file_path, email, uploader_url, allow_for_analysis = 'false', obfuscated = 'false'):
-	
-    files = {'filearg': open(file_path,'rb')}
-    values = {'description': description, 'feedback': additional_feedback, 'email': email, 'allowForAnalysis': allow_for_analysis, 'obfuscated': obfuscated}
-    r = requests.post(uploader_url, files = files, data = values)
+def create_list():
+    """
+    Функция поиска новых логов на PX4.
+    Тк пикс не создает супервложенных директорий,
+    можно обойтись без рекурсивного перехода между папками
 
-    return r.status_code, r.url
+    при использовании нужна блокировка, тк ресурс не может работать параллельно
+    with mavftp_lock:
+    """
 
+    """
+    Функция поиска новых логов на PX4.
+    Тк пикс не создает супервложенных директорий,
+    можно обойтись без рекурсивного перехода между папками
+    """
+    while True:
+        try:
+            with mavftp_lock:
 
-def load_param(param, default=None):
-    if rospy.has_param(param):
-        return rospy.get_param(param)
-    elif not default is None:
-        print("Param: " + str(param) + " not set & use default value: " + str(default))
-        return rospy.get_param(param, default)
-    else:
-        print("Error: " + str(param) + " not set & have not default value")
-        raise SystemExit
+                accept_operation.wait()
+
+                time.sleep(1) # чтобы поток успел умереть
+                print("Ищу новые логи...")
+                logs_folder = ftp_list_call("/fs/microsd/log")
+                if logs_folder.success and logs_folder.r_errno == 0:
+
+                    # проход по папкам с файлами логов
+                    for logs_folder_list in logs_folder.list:
+
+                        # если type равен 0, значит это файл, если 1, то это папка
+                        if logs_folder_list.type == 1:
+
+                            path_session_logs_folder = "/fs/microsd/log/" + logs_folder_list.name
+
+                            session_logs_folder = ftp_list_call(path_session_logs_folder)
+                            if session_logs_folder.success and session_logs_folder.r_errno == 0:
+
+                                for log in session_logs_folder.list:
+                                    if (log.type == 0):
+                                        # если найден файл лога
+                                        log_path = path_session_logs_folder + "/" + log.name
+                                        if not db.is_log_in_records(log_path): db.on_find(log_path)
+
+                            else: print("Import log folder error in " + path_session_logs_folder)
+                else: print("Import log folder error in /fs/microsd/log")
+        except Exception as ex:
+            print("Finder error: " + str(ex))
+    logs_dict=load_json(JSON_PATH)
+    
+    # если type равен 0 значит это файл, если 1 то это папка
+    # тк пикс не создает супервложенных директорий, можно обойтись без рекурсивного перехода между папками
+    main_logs_folder=ftp_list_call('/fs/microsd/log')
+    if(main_logs_folder.success and main_logs_folder.r_errno == 0):
+        for main_logs_folder_list in main_logs_folder.list:
+            if (main_logs_folder_list.type == 1):
+                path_current_folder_of_logs="/fs/microsd/log/" + main_logs_folder_list.name
+                # проход по папкам с файлами логов
+                current_folder_of_logs=ftp_list_call(path_current_folder_of_logs)
+                if(current_folder_of_logs.success and current_folder_of_logs.r_errno == 0):
+                    for log in current_folder_of_logs.list:
+                        if (log.type == 0): # если найден файл лога
+                            _log_path = path_current_folder_of_logs + "/" + log.name
+                            if not is_log_in_dict(_log_path,logs_dict):
+                                logs_dict.append({"path_on_px4":_log_path,"path_on_rpi":"","downloaded":False,"uploaded":False,"url_px4io":""})
+                else: print("Import log folder error in " + path_current_folder_of_logs)
+    else: print("Import log folder error in /fs/microsd/log")
+    dump_json(logs_dict,JSON_PATH)
+
+    logs_folder = ftp_list_call("/fs/microsd/log")
+                if logs_folder.success and logs_folder.r_errno == 0:
+
+                    # проход по папкам с файлами логов
+                    for logs_folder_list in logs_folder.list:
+
+                        # если type равен 0, значит это файл, если 1, то это папка
+                        if logs_folder_list.type == 1:
+
+                            path_session_logs_folder = "/fs/microsd/log/" + logs_folder_list.name
+
+                            session_logs_folder = ftp_list_call(path_session_logs_folder)
+                            if session_logs_folder.success and session_logs_folder.r_errno == 0:
+
+                                for log in session_logs_folder.list:
+                                    if (log.type == 0):
+                                        # если найден файл лога
+                                        log_path = path_session_logs_folder + "/" + log.name
+                                        if not db.is_log_in_records(log_path): db.on_find(log_path)
+
+                            else: print("Import log folder error in " + path_session_logs_folder)
+                else: print("Import log folder error in /fs/microsd/log")
 
 
 def main():
     rospy.init_node('px4logs_manager')
+    # rospy.init_node('creator_list_px4_ulog')
+
+    JSON_PATH='logs/px4logs.json'
+
+    # /mavros/ftp/checksum
+    # /mavros/ftp/close
+    # /mavros/ftp/list
+    # /mavros/ftp/mkdir
+    # /mavros/ftp/open
+    # /mavros/ftp/read
+    # /mavros/ftp/remove
+    # /mavros/ftp/rename
+    # /mavros/ftp/reset
+    # /mavros/ftp/rmdir
+    # /mavros/ftp/truncate
+    # /mavros/ftp/write
+
+    # pi@raspberrypi:~ $ rosservice  
+    # args  call  find  info  list  type  uri
+
+    # флешки, нет в px
+    # pi@raspberrypi:~ $ rosservice call /mavros/ftp/list /fs/microsd/log
+    # list: []
+    # success: True
+    # r_errno: 0
+
+    ftp_list_call = rospy.ServiceProxy('/mavros/ftp/list', FileList)
+    # rosservice call /mavros/ftp/list /fs/microsd/log
 
     no_progressbar = False
     try:
@@ -369,91 +362,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:set ts=4 sw=4 et:
-
-import rospy
-import json
-from mavros_msgs.srv import FileList
-
-rospy.init_node('creator_list_px4_ulog')
-
-JSON_PATH='logs/px4logs.json'
-
-# /mavros/ftp/checksum
-# /mavros/ftp/close
-# /mavros/ftp/list
-# /mavros/ftp/mkdir
-# /mavros/ftp/open
-# /mavros/ftp/read
-# /mavros/ftp/remove
-# /mavros/ftp/rename
-# /mavros/ftp/reset
-# /mavros/ftp/rmdir
-# /mavros/ftp/truncate
-# /mavros/ftp/write
-
-# pi@raspberrypi:~ $ rosservice  
-# args  call  find  info  list  type  uri
-
-# флешки, нет в px
-# pi@raspberrypi:~ $ rosservice call /mavros/ftp/list /fs/microsd/log
-# list: []
-# success: True
-# r_errno: 0
-
-ftp_list_call = rospy.ServiceProxy('/mavros/ftp/list', FileList)
-# rosservice call /mavros/ftp/list /fs/microsd/log
-
-def is_log_in_dict(log_path,logs_dict):
-    # ищем такой же лог в словаре
-    for _log in logs_dict:
-        # в структуре может отсуствовать name
-        if (_log['path_on_px4'] == log_path): return True
-        # отлов ошибки нужно сделать "AttributeError: 'dict' object has no attribute 'name'"
-    return False
-    
-def load_json(path):
-    with open(path, 'r') as infile:
-        _dict=json.load(infile)
-        # надо поставить оотлов ошибки, если файла нет или в нем невалидный !"[]" json "ValueError: No JSON object could be decoded"
-    infile.close()
-    return _dict
-
-def dump_json(data,path):
-    with open(path, 'w') as outfile:
-        json.dump(data, outfile)
-    outfile.close
-
-def create_list():
-    logs_dict=load_json(JSON_PATH)
-    
-    # если type равен 0 значит это файл, если 1 то это папка
-    # тк пикс не создает супервложенных директорий, можно обойтись без рекурсивного перехода между папками
-    main_logs_folder=ftp_list_call('/fs/microsd/log')
-    if(main_logs_folder.success and main_logs_folder.r_errno == 0):
-        for main_logs_folder_list in main_logs_folder.list:
-            if (main_logs_folder_list.type == 1):
-                path_current_folder_of_logs="/fs/microsd/log/" + main_logs_folder_list.name
-                # проход по папкам с файлами логов
-                current_folder_of_logs=ftp_list_call(path_current_folder_of_logs)
-                if(current_folder_of_logs.success and current_folder_of_logs.r_errno == 0):
-                    for log in current_folder_of_logs.list:
-                        if (log.type == 0): # если найден файл лога
-                            _log_path = path_current_folder_of_logs + "/" + log.name
-                            if not is_log_in_dict(_log_path,logs_dict):
-                                logs_dict.append({"path_on_px4":_log_path,"path_on_rpi":"","downloaded":False,"uploaded":False,"url_px4io":""})
-                else: print("Import log folder error in " + path_current_folder_of_logs)
-    else: print("Import log folder error in /fs/microsd/log")
-    dump_json(logs_dict,JSON_PATH)
-
-def main():
-    create_list()
-    logs_dict=load_json(JSON_PATH)
-    print logs_dict
-
-if __name__ == '__main__':
-    main()
-    
