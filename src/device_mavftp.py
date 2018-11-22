7eaae7e4db4d153b315bbee9f7e8238f270d100d
Artem Smirnov <urpylka@gmail.com>
2018-11-22 14:21:03 +0300

    Some updates

--- src/device_mavftp.py
+++ src/device_mavftp.py
@@ -22,7 +22,6 @@ import time, sys, os
 from threading import Thread, Lock
 
 import rospy, mavros
-from mavros.nuttx_crc32 import *
 # from mavros.utils import *
 from mavros_msgs.srv import FileList
 
@@ -86,10 +85,9 @@ class ProgressBar:
 #         return os.path.normpath(os.path.join(pwd, path))
 
 
-class PX4LOGS(Device):
-    """
-    source = PX4LOGS("/fs/microsd/log")
-    """
+class MAVFTP(Device):
+
+    ftp_list = rospy.ServiceProxy('/mavros/ftp/list', FileList)
 
     def __init__(self, *args):
         self.logdir = args
@@ -110,43 +108,14 @@ class PX4LOGS(Device):
 
     def download(self, remote_path, local_path):
     """
-
-    Posible errors:
-
-    может быть ошибка что флешка на пиксе не доступна (ошибка 110 например)
-    закрыть поток на ftp "rosservice call /mavros/ftp/close NAME_OF_FILE" & сбросить ftp "rosservice call /mavros/ftp/reset"    
-
-    AttributeError: __exit__
-    mavftp.do_download_local("/fs/microsd/log/2017-10-20/10_53_02.ulg","logs/10_53_02.ulg",True,True,True)
-
-    донладим (по хорошему нужна проверка что лог уже не скачен и имя не повторяется)
-    rosrun mavros mavftp download /fs/microsd/log/2017-10-20/13_29_53.ulg 13_29_53.ulg
-    
-    pi@raspberrypi:~/urpylka_px4logs $ rosservice call /mavros/ftp/open /fs/microsd/log/2017-10-20/10_53_02.ulg 0
-    size: 1676016
-    success: True
-    r_errno: 0
-    
-    pi@raspberrypi:~/urpylka_px4logs $ rosservice call /mavros/ftp/read /fs/microsd/log/2017-10-20/10_53_02.ulg 0 1676015
-    data: []
-    success: False
-    r_errno: 5
-    
-    ошибка 9 возникает когда файл не открыт
-    
-    pi@raspberrypi:~/urpylka_px4logs $ rosservice call /mavros/ftp/read /fs/microsd/log/2017-10-20/10_53_02.ulg 0 100
-    data: [85, 76, 111, 103, 1, 18, 53, 1, 58, 201, 155, 24, 0, 0, 0, 0, 40, 0, 66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56, 0, 73, 15, 99, 104, 97, 114, 91, 52, 48, 93, 32, 118, 101, 114, 95, 115, 119, 53, 56, 50, 48, 98, 102, 98, 55, 57, 52, 48, 101, 99, 54, 57, 53, 55, 51, 99, 97, 101, 51]
-    success: True
-    r_errno: 0
-    
-    кароче качать можно и нужно вообще побайтно (в принципе как я понял можно уже в полете), но туповато это самому реализовывать поэтому возьму это из mavftp
-
+    кароче качать можно и нужно вообще побайтно (в принципе как я понял можно уже в полете),
+    но туповато это самому реализовывать поэтому возьму это из mavftp
     """
 
-    verbose = True
-    verify = True
-
     with mavftp_lock:
+
+        verbose = True
+        verify = True
 
         # optimized transfer size for FTP message payload
         # XXX: bug in ftp.cpp cause a doubling request of last package.
@@ -177,7 +146,7 @@ class PX4LOGS(Device):
                     if len(buf) == 0:
                         break
 
-                    local_crc = nuttx_crc32(buf, local_crc)
+                    local_crc = mavros.nuttx_crc32(buf, local_crc)
                     local_file.write(buf)
                     bar.update(remote_file.tell())
 
@@ -185,57 +154,82 @@ class PX4LOGS(Device):
                         print_if(verbose, "Verifying...")
                         remote_crc = mavros.ftp.checksum(file_path)
                         if local_crc != remote_crc:
-                            # fault("Verification failed: 0x{local_crc:08x} != 0x{remote_crc:08x}".format(**locals()))
                             raise Exception("Verification failed: 0x{local_crc:08x} != 0x{remote_crc:08x}".format(**locals()))
-                
-                return remote_file.size
+
+        # Posible errors:
+
+        # может быть ошибка что флешка на пиксе не доступна (ошибка 110 например)
+        # закрыть поток на ftp "rosservice call /mavros/ftp/close NAME_OF_FILE" & сбросить ftp "rosservice call /mavros/ftp/reset"    
+
+        # AttributeError: __exit__
+        # mavftp.do_download_local("/fs/microsd/log/2017-10-20/10_53_02.ulg","logs/10_53_02.ulg",True,True,True)
+
+        # донладим (по хорошему нужна проверка что лог уже не скачен и имя не повторяется)
+        # rosrun mavros mavftp download /fs/microsd/log/2017-10-20/13_29_53.ulg 13_29_53.ulg
+        
+        # pi@raspberrypi:~/urpylka_px4logs $ rosservice call /mavros/ftp/open /fs/microsd/log/2017-10-20/10_53_02.ulg 0
+        # size: 1676016
+        # success: True
+        # r_errno: 0
+        
+        # pi@raspberrypi:~/urpylka_px4logs $ rosservice call /mavros/ftp/read /fs/microsd/log/2017-10-20/10_53_02.ulg 0 1676015
+        # data: []
+        # success: False
+        # r_errno: 5
+        
+        # ошибка 9 возникает когда файл не открыт
+        
+        # pi@raspberrypi:~/urpylka_px4logs $ rosservice call /mavros/ftp/read /fs/microsd/log/2017-10-20/10_53_02.ulg 0 100
+        # data: [85, 76, 111, 103, 1, 18, 53, 1, 58, 201, 155, 24, 0, 0, 0, 0, 40, 0, 66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56, 0, 73, 15, 99, 104, 97, 114, 91, 52, 48, 93, 32, 118, 101, 114, 95, 115, 119, 53, 56, 50, 48, 98, 102, 98, 55, 57, 52, 48, 101, 99, 54, 57, 53, 55, 51, 99, 97, 101, 51]
+        # success: True
+        # r_errno: 0
+
 
         except Exception as ex:
-            raise Exception("Download error: " + str(ex))
             mavros.ftp.reset_server()
+            raise Exception("Download error: " + str(ex))
+        finally:
+            local_file.close()
+
+        return remote_file.size
 
 
 def get_list(self):
-    """
-    Функция поиска новых логов на PX4.
-    Тк пикс не создает супервложенных директорий,
-    можно обойтись без рекурсивного перехода между папками
-    """
-
-    ftp_list_call = rospy.ServiceProxy('/mavros/ftp/list', FileList)
+    root_path = "/fs/microsd/log"
+    array = []
     while True:
         try:
             with mavftp_lock:
 
-                logs_folder = ftp_list_call("/fs/microsd/log")
+                logs_folder = self.ftp_list(root_path)
                 if not logs_folder.success or logs_folder.r_errno != 0:
-                    raise Exception("Import log folder error in /fs/microsd/log")
+                    raise Exception("Import log folder error in " + root_path)
                 else:
                     # проход по корневой папке /fs/microsd/log
                     for logs_folder_list in logs_folder.list:
 
                         # если type равен 0, значит это файл, если 1, то это папка
+                        # тк пикс не создает супервложенных директорий для логов,
+                        # можно обойтись без рекурсивного перехода между папками
                         if logs_folder_list.type == 1:
 
-                            path_session_logs_folder = "/fs/microsd/log/" + logs_folder_list.name
+                            path_session_logs_folder = root_path + '/' + logs_folder_list.name
 
                             # проход по папкам с файлами логов
-                            session_logs_folder = ftp_list_call(path_session_logs_folder)
+                            session_logs_folder = self.ftp_list(path_session_logs_folder)
 
                             if not session_logs_folder.success or session_logs_folder.r_errno != 0:
                                 raise Exception("Import log folder error in " + path_session_logs_folder)
                             else:
                                 for log in session_logs_folder.list:
-                                    if (log.type == 0):
-                                        # если найден файл лога
-                                        log_path = path_session_logs_folder + "/" + log.name
 
-                                        if not db.is_log_in_records(log_path):
-                                            db.on_find(log_path)
+                                    # если найден файл лога
+                                    if log.type == 0:
+                                        array.append(path_session_logs_folder + "/" + log.name)
 
         except Exception as ex:
             raise Exception("Finder error: " + str(ex))
-
+        return array
 
 def main():
 
