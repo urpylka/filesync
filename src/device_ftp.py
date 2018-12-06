0dc483984b46b7e14904f9d50b17f6e51a6241ea
Artem Smirnov <urpylka@gmail.com>
2018-12-06 16:51:54 +0300

    Debug

--- src/device_ftp.py
+++ src/device_ftp.py
@@ -85,10 +85,18 @@ class FTP(Device):
 
     def stream_upload(self, source_stream, device_path, chunk_size=1024):
         self.is_remote_available.wait()
+
         with self._internal_lock:
+            self._logger.info("0")
 
             self._ftp.cwd('/')
+            self._logger.info("1")
             res = self._ftp.storbinary('STOR ' + '/' + device_path, source_stream)
+
+            self._logger.info("2")
 
             if not res.startswith('226 Transfer complete'):
                 raise Exception("File was not uploaded successful: " + res)
+            
+        
+        self._logger.info("3")
